49f1653155efe743a0e6626b29e0dd914ed6f8eb
Rust Talapov <rust.talapov@gmail.com>
2018-04-25 11:30:36 +0300

    initial commit

---
+++ src/web.entry.js
@@ -0,0 +1,19 @@
+import './style.scss';
+import moment from 'moment-timezone';
+
+moment.tz.setDefault('UTC');
+
+let events = window.__INITIAL_STATE__.map(event => {
+  return {
+    description: event.description,
+    date: moment(event.date)
+  }
+});
+
+import VueCalendar from './entry';
+
+setTimeout(function () {
+  VueCalendar(events).$mount('#app');
+}, 2000);
+
+
